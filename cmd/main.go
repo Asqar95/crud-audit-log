cc854ea0b8c735246103e9bdf07b91c299dad708
Asqar <Asqar95>
2023-06-26 19:42:02 +0500

    start app

---
+++ cmd/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/Asqar95/crud-audit-log.git/internal/config"
+	"github.com/Asqar95/crud-audit-log.git/internal/repository"
+	"github.com/Asqar95/crud-audit-log.git/internal/server"
+	"github.com/Asqar95/crud-audit-log.git/internal/service"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func main() {
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	opts := options.Client()
+	opts.SetAuth(options.Credential{
+		Username: cfg.DB.Username,
+		Password: cfg.DB.Password,
+	})
+	opts.ApplyURI(cfg.DB.URI)
+
+	dbClient, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := dbClient.Ping(context.Background(), nil); err != nil {
+		log.Fatal(err)
+	}
+
+	db := dbClient.Database(cfg.DB.Database)
+
+	auditRepo := repository.NewAudit(db)
+	auditService := service.NewAudit(auditRepo)
+	auditSrv := server.NewAuditServer(auditService)
+	srv := server.New(auditSrv)
+
+	fmt.Println("SERVER STARTED", time.Now())
+
+	if err := srv.ListenAndServe(cfg.Server.Port); err != nil {
+		log.Fatal(err)
+	}
+}
